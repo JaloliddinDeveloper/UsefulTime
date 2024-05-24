5cdb00c832d2cf82fecb7a6d75b59477c7504128
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-05-25 00:32:27 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync->PASS

--- UsefulTime.Api/Services/Foundations/VedioMetadatas/VideoMetadataService.Exceptions.cs
+++ UsefulTime.Api/Services/Foundations/VedioMetadatas/VideoMetadataService.Exceptions.cs
@@ -2,6 +2,7 @@
 //Copyright (c) Coalition of Good-Hearted Engineers 
 //Free To Use To Find Comfort and Pease
 //=================================================
+using Microsoft.Data.SqlClient;
 using UsefulTime.Api.Models.VideoMetadatas;
 using UsefulTime.Api.Models.VideoMetadatas.Exceptions;
 using Xeptions;
@@ -25,7 +26,26 @@ namespace UsefulTime.Api.Services.Foundations.VideoMetadatas
             {
                 throw CreateAndLogValidationException(invalidVideoMetadataException);
             }
+            catch (SqlException sqlException )
+            {
+                var failedVideoMetadataStorageException =
+                new FailedVideoMetadataStorageException(
+                    message: "Failed video metadata error occured, cotact support",
+                    innerException: sqlException);
+                throw CreateAndLogCriticalDependencyException(failedVideoMetadataStorageException);
+            }
         }
+
+        private VideoMetadataDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var videoMetadataDependencyException =
+              new VideoMetadataDependencyException(
+                  message: "Video metadata error occured, fix the errors and try again",
+                  innerException: exception);
+            this.loggingBroker.LogCritical(videoMetadataDependencyException);
+            throw videoMetadataDependencyException;
+        }
+
         private VideoMetadataValidationException CreateAndLogValidationException(Xeption exception)
         {
             var videoMetadataValidationException =
