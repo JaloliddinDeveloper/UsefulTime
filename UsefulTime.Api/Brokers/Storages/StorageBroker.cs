afe3526c126e73b4bdbec391eec492514c38eea0
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-05-24 20:16:00 +0500

    DATA: Model & Migrate Video metadata

---
+++ UsefulTime.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,27 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers 
+//Free To Use To Find Comfort and Pease
+//=================================================
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsefulTime.Api.Brokers.Storages
+{
+    public partial class StorageBroker:EFxceptionsContext,IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Database.Migrate();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+               this.configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
