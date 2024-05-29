bdc7c412bb62253d382417d69f880b8d9cc12011
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-05-29 13:23:57 +0500

    BROKERS: DateTimeBroker

---
+++ UsefulTime.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,12 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers 
+//Free To Use To Find Comfort and Pease
+//=================================================
+namespace UsefulTime.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker:IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
